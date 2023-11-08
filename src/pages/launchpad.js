b55907316321bcbf9dde0b57135ecc82db916c08
Scroll Sale <149031380+Scrollsale@users.noreply.github.com>
2023-11-08 12:50:07 +0800

    Update launchpad.js

--- src/pages/launchpad.js
+++ src/pages/launchpad.js
@@ -23,7 +23,7 @@ const Launchpad = (props) => {
 
   return (
     <s.Container ai="center">
-      <s.TextTitle>Launchpad</s.TextTitle>
+      <s.TextTitle>Shibpad</s.TextTitle>
       <s.SpacerMedium />
       <TextField
         fullWidth
