56539685983e71141d4962ff85d2de5cbfc341e1
Scroll Sale <149031380+Scrollsale@users.noreply.github.com>
2023-11-09 09:56:03 +0800

    Update global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -84,7 +84,7 @@ export const BodyWrapper = styled.div`
   box-shadow: rgba(0, 0, 0, 0.01) 0px 0px 1px, rgba(0, 0, 0, 0.04) 0px 4px 8px, rgba(0, 0, 0, 0.04) 0px 16px 24px,
   rgba(0, 0, 0, 0.01) 0px 24px 32px;
 
-  background-color: #592C00;
+  background-color: #EEAD6F;
 
   @media (max-width: 540px) {
     width: 90%;
@@ -110,7 +110,7 @@ export const LoaderWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #592C00;
+  background-color: #EEAD6F;
 `;
 
 export const IconWrapper = styled.div`
